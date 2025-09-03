2923e07e0817e2070c56148782754956d5ef5a32
Devendra Kumar Narware <devn2405@gmail.com>
2025-09-03 18:50:54 +0530

    Add files via upload

---
+++ N_Queen.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class N_Queen {
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // vertical
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void nQueen(char board[][], int row) {
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (isSafe(board, row, i)) {
+                board[row][i] = 'Q';
+                nQueen(board, row + 1);
+                board[row][i] = 'X';
+            }
+        }
+
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("-----Chess Board-----");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter number:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char board[][] = new char[n][n];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        nQueen(board, 0);
+    }
+}
