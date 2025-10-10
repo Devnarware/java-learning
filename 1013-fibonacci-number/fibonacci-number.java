951a204c4ee5976743e96cb48590327122d11d57
Devendra Kumar Narware <devn2405@gmail.com>
2025-10-10 16:25:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (28.85%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,17 @@
 class Solution {
     public int fib(int n) {
-        if(n < 2){
-            return n ;
+        int fibz = 0;
+        int fibo = 1;
+        int fib = 1;
+        if (n == 0) {
+            return 0;
+        } else {
+            for (int i = 2; i <= n; i++) {
+                fib = fibo + fibz;
+                fibz = fibo;
+                fibo = fib;
+            }
         }
-        return fib(n-1) + fib(n-2) ;
+        return fib;
     }
 }
