250b011551cd72f7936f2c2f97ac20094f7fe27b
Devendra Kumar Narware <devn2405@gmail.com>
2025-10-10 15:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (79.22%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,18 +1,24 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int si = 0 ; 
-        int ei = arr.length - 1 ;
-        int mid ;
-        while(si<=ei){
-            mid = si + (ei - si) / 2 ;
-            if(arr[mid] == target){
-                return mid ;
-            }else if(arr[mid] < target){
-                si = mid + 1 ;
-            }else{
-                ei = mid - 1 ;
-            }
+    public static int bSearch(int[] arr, int key, int si, int ei) {
+        if(si>ei){
+            return -1 ;
         }
-        return -1 ;
+        int mid = si + (ei - si)/2 ;
+        if(arr[mid] == key){
+            return mid ;
+        }
+        if(arr[mid]<key){
+            return bSearch(arr , key , mid+1 , ei) ;
+        }else{
+            return bSearch(arr , key , si , mid-1) ;
+
+        }
+    }
+
+    public int search(int[] arr, int target) {
+        int si = 0;
+        int ei = arr.length - 1;
+        return bSearch(arr, target, si, ei);
+
     }
 }
