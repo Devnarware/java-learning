f03f6e76066b823e12b09def64cc6de5eb7f6d96
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-10 01:26:47 +0530

    Learning about segment tree

--- learning/Trees/SegmentTree/ST.java
+++ learning/Trees/SegmentTree/ST.java
@@ -2,31 +2,48 @@ package SegmentTree;
 
 public class ST {
 
-    private static class Node{
+    private static class Node {
 
-        int data ;
-        int startInterval ;
-        int endInterval ;
-        Node left ;
-        Node right ;
+        int data;
+        int startInterval;
+        int endInterval;
+        Node left;
+        Node right;
 
-        public void Node(int startInterval, int endInterval){
-            this.startInterval = startInterval ;
-            this.endInterval = endInterval ;
+        public Node(int startInterval, int endInterval) {
+            this.startInterval = startInterval;
+            this.endInterval = endInterval;
         }
 
-        public Node root ;
     }
 
+    Node root;
 
-    public ST(int[] arr){
+
+    public ST(int[] arr) {
 
         // Create a tree
-//        this.root = constructTree(arr, 0, arr.length-1) ;
+        this.root = constructTree(arr, 0, arr.length-1) ;
     }
 
 
-    public void constructTree(int[] arr, int si, int ei){
-//        return Node ;
+    public Node constructTree(int[] arr, int start, int end) {
+
+        if(start == end){
+            Node node = new Node(start, end) ;
+            node.data = arr[start] ;
+            return node ;
+        }
+
+        Node node = new Node(start, end) ;
+
+        int mid = start + (end-start) / 2 ;
+
+        node.left = constructTree(arr, start, mid) ;
+        node.right = constructTree(arr, mid+1, end) ;
+
+        node.data = node.left.data + node.right.data ;
+
+        return node ;
     }
 }
