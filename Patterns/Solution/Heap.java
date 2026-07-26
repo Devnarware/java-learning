d1be78c02e36c1bf6d9efabf3965676b0f51934c
Devendra Kumar Narware <devn2405@gmail.com>
2026-07-26 13:19:40 +0530

    feat: implement quicksort algorithm

--- Patterns/Solution/Heap.java
+++ Patterns/Solution/Heap.java
@@ -50,11 +50,61 @@ class Heap {
         }
     }
 
+    private static void quicksort(int[] arr, int s, int e){
+        if(s >= e){
+            return ;
+        }
+
+        int low = s ;
+        int hi = e ;
+        int mid = low + (hi - low) / 2 ;
+        int pivot = arr[mid] ;
+
+        while(low <= hi){
+            while(arr[low] < pivot){
+                low++ ;
+            }
+
+            while(arr[hi] > pivot){
+                hi-- ;
+            }
+
+            if (low <= hi){
+                swap(arr, low, hi) ;
+
+                low++ ;
+                hi-- ;
+            }
+        }
+
+        quicksort(arr, s, hi);
+        quicksort(arr, low, e);
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i] ;
+        arr[i] = arr[j] ;
+        arr[j] = temp ;
+    }
+
+
     public static void main(String[] args) {
 
         int[] arr = {5,4,3,6,8,2};
-        mergeSort(arr, 0, arr.length - 1);
+        quicksort(arr, 0, arr.length - 1);
 
         System.out.println(Arrays.toString(arr));
+    }
+}
+
+class HeapSort{
+    ArrayList<Integer> list ;
+
+    public HeapSort(){
+        list = new ArrayList<>() ;
+    }
+
+    public void insert(int val){
+        
     }
 }
